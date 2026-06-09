70c24abc5b4d725dacb120ddeec602dc63543f35
NeuroDev204 <phamvansy204@gmail.com>
2026-06-10 00:55:17 +0700

    fix register workflow v2

--- Backend/user-service/src/main/java/com/blur/userservice/profile/configuration/KeycloakConfig.java
+++ Backend/user-service/src/main/java/com/blur/userservice/profile/configuration/KeycloakConfig.java
@@ -1,10 +1,14 @@
 package com.blur.userservice.profile.configuration;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class KeycloakConfig {
@@ -23,12 +27,19 @@ public class KeycloakConfig {
 
     @Bean
     public Keycloak keycloak() {
+        ResteasyClient httpClient = (ResteasyClient) ResteasyClientBuilder.newBuilder()
+                .connectionPoolSize(10)
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(15, TimeUnit.SECONDS)
+                .build();
+
         return KeycloakBuilder.builder()
                 .serverUrl(serverUrl)
                 .realm(realm)
                 .clientId(clientId)
                 .clientSecret(clientSecret)
                 .grantType("client_credentials")
+                .resteasyClient(httpClient)
                 .build();
     }
 }
